bc84b43a5e5d34dfffe848e42cd91e170f4a83cc
mrajo <anthony.c@stle.co>
2016-03-23 16:35:17 -0700

    add more basic config tests

--- src/plugin.js
+++ src/plugin.js
@@ -9,10 +9,11 @@ const debug = require('debug')('metalsmith-michaelangelo')
 const plugin = (params) => {
   const defaults = {
     directory: 'templates',
-    templateKey: 'layout',
+    templateKey: 'template',
     default: 'default.html',
     pattern: '**/*.html',
-    filters: {}
+    filters: {},
+    debug: false
   }
   const options = Object.assign(defaults, params)
 
@@ -20,18 +21,23 @@ const plugin = (params) => {
     const env = nunjucks.configure(metalsmith.path(options.directory))
 
     env.addFilter('rot13', (data) => {
-      if (typeof data == "string") {
+      if (typeof data == 'string') {
         return data.replace(/[a-zA-Z]/g, (c) => {
-          return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+          return String.fromCharCode((c <= 'Z' ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26)
         })
       }
       return data
     });
 
+    const should_render = (file) => {
+      return (options.pattern && match(file, options.pattern)[0]) ||
+             (options.pattern == null && files[file].hasOwnProperty(options.templateKey) && files[file][options.templateKey] != null)
+    }
+
     const render = (file, done) => {
-      if (options.pattern && match(file, options.pattern)[0]) {
+      if (should_render(file)) {
         debug(`rendering ${file}`)
-        const template = metalsmith.path(options.directory, files[file].layout || options.default)
+        const template = metalsmith.path(options.directory, files[file][options.templateKey] || options.default)
         const data = Object.assign({}, metalsmith.metadata(), files[file])
         data.contents = files[file].contents.toString()
         const rendered = env.render(template, data)
@@ -43,7 +49,15 @@ const plugin = (params) => {
       }
     }
 
-    each(Object.keys(files), render, done)
+    each(Object.keys(files), render, (err) => {
+      if (options.debug) {
+        debug(`creating debug file`)
+        files['debug.json'] = {
+          contents: new Buffer(JSON.stringify(options))
+        }
+      }
+      done()
+    })
   }
 }
 
--- test/test.js
+++ test/test.js
@@ -4,25 +4,96 @@ var assert = require('assert');
 var dir_equal = require('assert-dir-equal');
 var Metalsmith = require('metalsmith');
 var render = require('../src/plugin');
- var join = require('path').join;
-// var read = require('fs').readFileSync;
-// var buffer_equal = require('buffer-equal');
-// var utf8 = require('is-utf8');
+var path = require('path');
+var fs = require('fs');
+var buffer_equal = require('buffer-equal');
+var utf8 = require('is-utf8');
 
 function assertDirsEqual(src, done) {
   return function (err) {
     if (err) return done(err);
-    dir_equal(join(src, 'expected'), join(src, 'build'));
+    dir_equal(path.join(src, 'expected'), path.join(src, 'build'));
     done();
   };
 }
 
-describe('metalsmith-michaelangelo', function () {
+function assertFilesEqual(src, file, done) {
+  return function (err) {
+    var file_a = fs.readFileSync(path.join(src, 'expected', file));
+    var file_b = fs.readFileSync(path.join(src, 'build', file));
+
+    if (utf8(file_a) && utf8(file_b)) {
+      assert.equal(file_a.toString(), file_b.toString());
+    } else {
+      assert(buffer_equal(file_a, file_b));
+    }
+
+    done();
+  };
+}
+
+describe('configuration', function () {
   it('should use default configuration', function (done) {
-    var src = 'test/fixtures/defaults';
+    var src = 'test/fixtures/config-defaults';
 
     Metalsmith(src)
       .use(render())
       .build(assertDirsEqual(src, done));
   });
+
+  it('should override debug and create a debug output file', function (done) {
+    var src = 'test/fixtures/config-debug';
+
+    Metalsmith(src)
+      .use(render({
+        debug: true
+      }))
+      .build(assertFilesEqual(src, 'debug.json', done));
+  });
+
+  it('should override directory', function (done) {
+    var src = 'test/fixtures/config-directory';
+
+    Metalsmith(src)
+      .use(render({
+        directory: 'layouts',
+        debug: true
+      }))
+      .build(assertFilesEqual(src, 'debug.json', done));
+  });
+
+  it('should override templateKey', function (done) {
+    var src = 'test/fixtures/config-templatekey';
+
+    Metalsmith(src)
+      .use(render({
+        templateKey: 'layout',
+        debug: true
+      }))
+      .build(assertFilesEqual(src, 'debug.json', done));
+  });
+
+  it('should override default template', function (done) {
+    var src = 'test/fixtures/config-default-template';
+
+    Metalsmith(src)
+      .use(render({
+        default: 'main.tpl',
+        debug: true
+      }))
+      .build(assertFilesEqual(src, 'debug.json', done));
+  });
+
+  it('should override pattern', function (done) {
+    var src = 'test/fixtures/config-pattern';
+
+    Metalsmith(src)
+      .use(render({
+        pattern: '*.html',
+        debug: true
+      }))
+      .build(assertFilesEqual(src, 'debug.json', done));
+  });
+
+  it('should add filters');
 });
